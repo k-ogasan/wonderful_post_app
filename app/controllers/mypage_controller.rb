890c02370187786e6e3126bf3868f9a673d14f9c
小川航生 <>
2025-04-01 21:33:07 +0900

    gem kaminariの導入とUIを整えた。

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,6 +1,6 @@
 class MypageController < ApplicationController
   before_action :authenticate_user!
   def show
-    @articles = current_user.articles
+    @articles = current_user.articles.page(params[:page]).per(10)
   end
 end
